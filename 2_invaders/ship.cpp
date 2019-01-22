743e48a2a281025cb9635d6590e4ef3d75a4076c
Paulfinex <sentius96@gmail.com>
2019-01-22 03:06:36 +0000

    Update

---
+++ 2_invaders/ship.cpp
@@ -0,0 +1,30 @@
+//ship.cpp
+#include "ship.h"
+using namespace sf;
+using namespace std;
+
+Ship::Ship() {};
+
+Ship::Ship(IntRect ir) : Sprite() {
+  _sprite = ir;
+  setTexture(spritesheet);
+  setTextureRect(_sprite);
+};
+
+void Ship::Update(const float &dt) {}
+
+//Define the ship deconstructor. 
+//Although we set this to pure virtual, we still have to define it.
+Ship::~Ship() = default;
+
+//ship.cpp
+Invader::Invader() : Ship() {}
+
+Invader::Invader(sf::IntRect ir, sf::Vector2f pos) : Ship(ir) {
+	setOrigin(16, 16);
+	setPosition(pos);
+}
+
+void Invader::Update(const float &dt) {
+	Ship::Update(dt);
+}
---
+++ 2_invaders/spaceinv.cpp
@@ -0,0 +1,64 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "ship.h"
+//...
+
+using namespace sf;
+using namespace std;
+std::vector<Ship *> ships;
+const Keyboard::Key controls[2] = {
+	Keyboard::Left,   
+	Keyboard::Right
+
+};
+
+sf::Texture spritesheet;
+sf::Sprite invader;
+const int gameWidth = 800;
+const int gameHeight = 600;
+void Load() {
+	if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
+	cerr << "Failed to load spritesheet!" << std::endl;
+	}
+	invader.setTexture(spritesheet);
+	Invader* inv = new Invader(sf::IntRect(0, 0, 32, 32), { 100,100 });
+	//ships.push_back(inv);
+}
+
+
+void Update(RenderWindow &window) {
+	// Reset clock, recalculate deltatime
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	// check and consume events
+	Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
+		}
+	}
+	for (auto &s : ships) {
+		s->Update(dt);
+	};
+}
+
+void Render(RenderWindow &window) {
+	// Draw Everything
+    window.draw(invader);
+	for (const auto s : ships) {
+		window.draw(*s);
+	}
+}
+
+int main() {
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
+	Load();
+	while (window.isOpen()) {
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
+	}
+	return 0;
+}
