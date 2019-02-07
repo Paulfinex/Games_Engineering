10db8259ed88cc546224754d8d32c91b5c8611d9
Paulfinex <sentius96@gmail.com>
2019-02-07 09:56:29 +0000

    l

--- lib_tile_level_loader/levelsystem.h
+++ lib_tile_level_loader/levelsystem.h
@@ -1,42 +1,41 @@
-levelsystem.h
 #pragma once
 
-#include SFMLGraphics.hpp
-#include memory
-#include string
-#include vector
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <string>
+#include <vector>
 
 #define ls LevelSystem
 
 class LevelSystem {
-public
- enum TILE { EMPTY, START, END, WALL, ENEMY, WAYPOINT };
-        
- static void loadLevelFile(const stdstring&,float tileSize=100.f);
- static void render(sfRenderWindow &window);
- static sfColor getColor(TILE t);
- static void setColor(TILE t, sfColor c);
- //Get Tile at grid coordinate
- static TILE getTile(sfVector2ul);
- //Get Screenspace coordinate of tile
- static sfVector2f getTilePosition(sfVector2ul);
- //get the tile at screenspace pos
- static TILE getTileAt(sfVector2f);
- 
-protected
- static stdunique_ptrTILE[] _tiles; //Internal array of tiles
- static size_t _width; //how many tiles wide is level
- static size_t _height; //how many tile high is level
- static sfVector2f _offset; //Screenspace offset of level, when rendered.
- static float _tileSize; //Screenspace size of each tile, when rendered.
- static stdmapTILE, sfColor _colours; //color to render each tile type
- 
- //array of sfml sprites of each tile
- static stdvectorstdunique_ptrsfRectangleShape _sprites;  
- //generate the _sprites array
- static void buildSprites();
-    
-private
- LevelSystem() = delete;
- ~LevelSystem() = delete;
+public:
+	enum TILE { EMPTY, START, END, WALL, ENEMY, WAYPOINT };
+
+	static void loadLevelFile(const std::string&, float tileSize = 100.f);
+	static void render(sf::RenderWindow &window);
+	static sf::Color getColor(TILE t);
+	static void setColor(TILE t, sf::Color c);
+	//Get Tile at grid coordinate
+	static TILE getTile(sf::Vector2f);
+	//Get Screenspace coordinate of tile
+	static sf::Vector2f getTilePosition(sf::Vector2f);
+	//get the tile at screenspace pos
+	static TILE getTileAt(sf::Vector2f);
+
+protected:
+	static std::unique_ptr<TILE[]> _tiles; //Internal array of tiles
+	static size_t _width; //how many tiles wide is level
+	static size_t _height; //how many tile high is level
+	static sf::Vector2f _offset; //Screenspace offset of level, when rendered.
+	static float _tileSize; //Screenspace size of each tile, when rendered.
+	static std::map<TILE, sf::Color> _colours; //color to render each tile type
+
+	//array of sfml sprites of each tile
+	static std::vector<std::unique_ptr<sf::RectangleShape>> _sprites;
+	//generate the _sprites array
+	static void buildSprites();
+
+private:
+	LevelSystem() = delete;
+	~LevelSystem() = delete;
 };
